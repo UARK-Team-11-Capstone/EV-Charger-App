63bbb1e05896e3a4319b32cfbde1aa0a9d59df11
gw014 <gw014@uark.edu>
2023-03-31 13:12:43 -0500

    Added new charger icon

--- EV Charger App/Views/MainPage.xaml.cs
+++ EV Charger App/Views/MainPage.xaml.cs
@@ -153,7 +153,7 @@ namespace EV_Charger_App
                         {
                             Type = PinType.Place,
                             Label = charger.station_name,
-                            Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("Battery-Icon.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "Battery-Icon.png", WidthRequest = 10, HeightRequest = 10 }),
+                            Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("icon_charger.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "Battery-Icon.png", WidthRequest = 10, HeightRequest = 10, Aspect = Aspect.AspectFit }),
                             Position = new Position(Convert.ToDouble(charger.latitude), Convert.ToDouble(charger.longitude)),
 
                         };
