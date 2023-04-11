0ccb3f4fc6b9b600cf730cd686c124edcdd2a228
Grant Wilkins <gw014@uark.edu>
2023-04-11 14:38:43 -0500

    Hooked up review charger screen to database. I still to grab the chargerID from the DOE API.

--- EV Charger App/Views/ReviewCharger.xaml.cs
+++ EV Charger App/Views/ReviewCharger.xaml.cs
@@ -32,6 +32,13 @@ namespace EV_Charger_App.Views
             String comment = CommentEditor.Text;
 
             // Save the review to the database
+            string email = GetUserFromToken();
+
+            //I need some way of getting the charger ID
+            string chargerID = "";
+
+            app.database.InsertRecord("Reviews", new string[4] { chargerID, email, rating.ToString(), comment });
+
             //await App.Database.SaveReviewAsync(rating, comment);
 
             // Show a confirmation message
@@ -40,6 +47,22 @@ namespace EV_Charger_App.Views
             // Reset the form
             RatingSlider.Value = 1;
             CommentEditor.Text = "";
+
+        }
+
+        string GetUserFromToken()
+        {
+            string email = "";
+
+            string token = app.session.getToken();
+
+            string query = "SELECT * FROM Users WHERE sessionToken = '" + token + "'";
+
+            List<Object[]> data = app.database.GetQueryRecords(query);
+
+            email = data[0][0].ToString();
+
+            return email;
         }
 
     }
